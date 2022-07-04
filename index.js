75f8f677a080bb27a9142c6e55ce29ff1a66accb
Andy <48938478+aott33@users.noreply.github.com>
2022-07-04 06:30:50 -0700

    tutorial completed

--- index.js
+++ index.js
@@ -5,4 +5,8 @@ const app = express()
 const PORT = 3000// Tell express to use body-parser's JSON parsing
 
 app.use(bodyParser.json())// Start express on the defined port
+app.post("/hook", (req, res) => {
+    console.log(req.body) // Call your action on the request here
+    res.status(200).end() // Responding is important
+})
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`))
